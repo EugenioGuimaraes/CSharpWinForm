86750887e111d11608458da007f719444a2a3e9f
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2023-07-26 11:00:04 -0300

    Fix: folders

---
+++ ModerDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ModerDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModerDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVm { get; set; }
+
+        private object _CurrentView;
+
+        public object CurrentView
+        {
+            get { return _CurrentView; }
+            set
+            {
+                _CurrentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+        }
+    }
+}   
