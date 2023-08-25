9a2ca5f46a5d34da5d2e05064e195ffe0c2b44d8
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2023-08-25 17:15:25 -0300

    Add: feat connection to DataBase

---
+++ WindowsFormsApplication1/DataBase/Db_Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.DataBase
+{
+    public abstract class Db_Connection
+    {
+        private readonly string connectionString;
+        
+        public Db_Connection()
+        {
+            connectionString = "Server=(local); DataBase=NorthwindStore; Integraded security=true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
