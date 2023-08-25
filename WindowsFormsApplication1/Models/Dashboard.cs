9590b863fa9b75a0ce956d8423715198b9e61114
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2023-08-25 17:29:17 -0300

    Add: feat Deashboard Models

---
+++ WindowsFormsApplication1/Models/Dashboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.DataBase.Models
+{
+
+    public struct RevenueByDate
+    {
+        public string Date { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+
+    public class Dashboard : Db_Connection
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+        private int numberDays;
+
+        public int NumCustomers { get; private set; }
+        public int NumSuppliers { get; private set; }
+        public int NumProducts { get; private set; }
+        public List<KeyValuePair<string, int>> TopProductList { get; private set; }
+        public List<KeyValuePair<string, int>> UnderstockList { get; private set; }
+        public List<RevenueByDate> GrossRevenueList { get; private set; }
+        public int NumOrders { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal TotalProfit { get; set; }
+    }
+}
